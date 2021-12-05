d7eb0c8cad2ac813c23309edb89bdb13f3c5375e
Ikenna Ogbajie <iogbajie@gmail.com>
2021-12-05 20:30:18 +0100

    aoc day1 cleanup

--- src/day1.rs
+++ src/day1.rs
@@ -1,9 +1,8 @@
 use std::fs;
-use std::convert::TryFrom;
 
 
 fn main() {
-    let sliding_window: i32 = 3;
+    let sliding_window: usize = 3;
 
     let file_path = "resources/day1-input.txt";
     let contents = fs::read_to_string(file_path)
@@ -26,11 +25,11 @@ fn day_1_part_1(input: Vec<i32>) {
     println!("{}", sum_increasing);
 }
 
-fn day_1_part_2(input: Vec<i32>, sliding_window: i32) {
+fn day_1_part_2(input: Vec<i32>, sliding_window: usize) {
     let mut sum_increasing = 0;
     let mut previous_value: i32 = 0;
     for (pos, value) in input.iter().enumerate() {
-        if (input.len() - pos) >= usize::try_from(sliding_window).unwrap() {
+        if (input.len() - pos) >= sliding_window {
             if (value + input[pos + 1] + input[pos + 2]) > previous_value && (previous_value != 0) {
                 sum_increasing += 1;
             }
