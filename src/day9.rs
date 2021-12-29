650d02d56d6aeb746425dcdfc573a4247f10ab52
idarlington <iogbajie@gmail.com>
2021-12-29 19:25:42 +0100

    Day 9 working

--- src/day9.rs
+++ src/day9.rs
@@ -1,4 +1,4 @@
-use std::collections::{HashMap, HashSet};
+use std::collections::{HashSet};
 use std::fs;
 
 #[derive(Debug, Clone)]
@@ -9,7 +9,6 @@ struct Basin {
 
 #[derive(Debug, Clone)]
 struct NextPoint {
-    depth: (i32, i32),
     point: (usize, usize),
 }
 
@@ -19,8 +18,7 @@ pub fn main() {
     let file_path = "resources/inputs/day9.txt";
     let contents = fs::read_to_string(file_path).expect("Something went wrong reading the file");
 
-
-    let heights = contents
+    let heights: Vec<Vec<u32>> = contents
         .split("\n")
         .collect::<Vec<&str>>()
         .iter()
@@ -33,7 +31,7 @@ pub fn main() {
         })
         .collect::<Vec<Vec<u32>>>();
 
-    let mut basins: Vec<Basin> =
+    let basins: Vec<Basin> =
         heights
             .clone()
             .iter()
@@ -65,13 +63,9 @@ pub fn main() {
                         };
 
                         if vertical && horizontal {
-                            let low_point = (line_pos, pos);
-                            let mut locations: HashSet<(usize, usize)> = HashSet::new();
-                            calculate_basin(low_point, &heights, (0, 0), &mut locations);
-
                             line_basins.push(Basin {
-                                low_point,
-                                locations,
+                                low_point: (line_pos, pos),
+                                locations: HashSet::new(),
                             });
                         }
 
@@ -80,191 +74,119 @@ pub fn main() {
                 )
             });
 
+    day_9_part_1(&basins, &heights);
+    day_9_part_2(&basins, &heights)
+}
+
+fn day_9_part_2(basins: &Vec<Basin>, heights: &Vec<Vec<u32>>) {
+    let mut basins = basins.clone();
+    let mut heights = heights.clone();
+    for (basin_pos, basin) in basins.clone().iter().enumerate() {
+        let mut locations: HashSet<(usize, usize)> = HashSet::new();
+        calculate_basin(basin.low_point, &mut heights, &mut locations);
+        basins[basin_pos] = Basin {
+            locations,
+            ..*basin
+        }
+    }
 
     basins.sort_by(|basin, other_basin| basin.locations.len().cmp(&other_basin.locations.len()));
     basins.reverse();
-
-
     println!(
-        "{} {} {}",
-        basins[0].locations.len(),
-        basins[1].locations.len(),
-        basins[2].locations.len()
-    );
-
-    println!(
-        "{:?}",
+        "Day 9, part 2: {}",
         basins[0].locations.len() * basins[1].locations.len() * basins[2].locations.len()
     );
-
-    println!("{:?}", basins);
 }
 
-fn day_9_part_1() {
-    let file_path = "resources/inputs/day9.txt";
-    let contents = fs::read_to_string(file_path).expect("Something went wrong reading the file");
-
-    let heights = contents
-        .split("\n")
-        .collect::<Vec<&str>>()
+fn day_9_part_1(basins: &Vec<Basin>, heights: &Vec<Vec<u32>>) {
+    let sum: u32 = basins
         .iter()
-        .map(|line| {
-            line.chars()
-                .collect::<Vec<char>>()
-                .iter()
-                .map(|height| height.to_digit(RADIX).unwrap())
-                .collect()
+        .map(|basin| {
+            let (line_pos, pos) = basin.low_point;
+            heights[line_pos][pos] + 1
         })
-        .collect::<Vec<Vec<u32>>>();
-
-    let low_points: Vec<u32> = heights.clone().iter().enumerate().fold(
-        Vec::new(),
-        |low_points, (line_pos, line_heights)| {
-            let size = line_heights.len();
-            line_heights.clone().iter().enumerate().fold(
-                low_points,
-                |mut line_low_points, (pos, height)| {
-                    let horizontal = {
-                        if pos == 0 {
-                            height < &line_heights[pos + 1]
-                        } else if pos == (size - 1) {
-                            height < &line_heights[pos - 1]
-                        } else {
-                            height < &line_heights[pos - 1] && height < &line_heights[pos + 1]
-                        }
-                    };
-
-                    let vertical = {
-                        if line_pos == 0 {
-                            height < &heights[line_pos + 1][pos]
-                        } else if line_pos == (heights.len() - 1) {
-                            height < &heights[line_pos - 1][pos]
-                        } else {
-                            height < &heights[line_pos + 1][pos]
-                                && height < &heights[line_pos - 1][pos]
-                        }
-                    };
-
-                    if vertical && horizontal {
-                        line_low_points.push(*height)
-                    }
-
-                    line_low_points.clone()
-                },
-            )
-        },
-    );
-
-    let sum: u32 = low_points
-        .iter()
-        .map(|low_point| low_point + 1)
         .collect::<Vec<u32>>()
         .iter()
         .sum();
 
-    println!("{:?}", sum);
+    println!("Day 9, part 1: {}", sum);
 }
 
 fn calculate_basin(
     current_position: (usize, usize),
-    heights: &Vec<Vec<u32>>,
-    calculation_depth: (i32, i32),
-    basins: &mut HashSet<(usize, usize)>,
+    heights: &mut Vec<Vec<u32>>,
+    locations: &mut HashSet<(usize, usize)>,
 ) {
-    let (line_depth, depth) = calculation_depth;
     let (line_pos, pos) = current_position;
     let (data_height, data_length) = (heights.len(), heights[0].len());
-    let current_value = heights[line_pos][pos];
 
-
-    basins.insert(current_position);
-    // get next elements
-    // for each of them, check they are higher than current, not edge, not 9
-    // get next elements for each item
-    // continue
+    locations.insert(current_position);
+    heights[current_position.0][current_position.1] = 9;
 
     // get next elements
     let mut next_points: Vec<NextPoint> = Vec::new();
 
     // check horizontal
-    if pos == 0 && depth == 0 {
-        if heights[line_pos][pos + 1] != 9 /*&& heights[line_pos][pos + 1] > current_value */ {
+    if pos == 0 {
+        if heights[line_pos][pos + 1] != 9 {
             next_points.push(NextPoint {
-                depth: (line_depth, depth + 1),
                 point: (line_pos, pos + 1),
             });
         }
-    } else if pos == (data_length - 1) && depth == 0 {
-        if heights[line_pos][pos - 1] != 9 /*&& heights[line_pos][pos - 1] > current_value*/ {
+    }
+
+    if pos == (data_length - 1) {
+        if heights[line_pos][pos - 1] != 9 {
             next_points.push(NextPoint {
-                depth: (line_depth, depth - 1),
                 point: (line_pos, pos - 1),
             });
         }
-    } else if pos != 0 && pos != (data_length - 1) {
-        if (depth.is_negative() || depth == 0)
-            && heights[line_pos][pos - 1] != 9
-        /*&& heights[line_pos][pos - 1] > current_value*/
-        {
+    }
+
+    if pos != 0 && pos != (data_length - 1) {
+        if heights[line_pos][pos - 1] != 9 {
             next_points.push(NextPoint {
-                depth: (line_depth, depth - 1),
                 point: (line_pos, pos - 1),
             });
         }
-        if (depth.is_positive() || depth == 0)
-            && heights[line_pos][pos + 1] != 9
-        /*&& heights[line_pos][pos + 1] > current_value*/
-        {
+        if heights[line_pos][pos + 1] != 9 {
             next_points.push(NextPoint {
-                depth: (line_depth, depth + 1),
                 point: (line_pos, pos + 1),
             });
         }
     }
 
     // check vertical
-    if line_pos == 0 && line_depth == 0 {
-        if heights[line_pos + 1][pos] != 9 /*&& heights[line_pos + 1][pos] > current_value*/ {
+    if line_pos == 0 {
+        if heights[line_pos + 1][pos] != 9 {
             next_points.push(NextPoint {
-                depth: (line_depth + 1, depth),
                 point: (line_pos + 1, pos),
             })
         }
-    } else if line_pos == (data_height - 1) && line_depth == 0 {
-        if heights[line_pos - 1][pos] != 9 /*&& heights[line_pos - 1][pos] > current_value*/ {
+    }
+
+    if line_pos == (data_height - 1) {
+        if heights[line_pos - 1][pos] != 9 {
             next_points.push(NextPoint {
-                depth: (line_depth - 1, depth),
-                point: (line_pos - 1, pos),
-            })
-        }
-    } else if line_pos != 0 && line_pos != (data_height - 1) {
-        if (line_depth.is_positive() || line_depth == 0)
-            && heights[line_pos + 1][pos] != 9
-        /*&& heights[line_pos + 1][pos] > current_value*/
-        {
-            next_points.push(NextPoint {
-                depth: (line_depth + 1, depth),
-                point: (line_pos + 1, pos),
-            })
-        }
-        if (line_depth.is_negative() || line_depth == 0)
-            && heights[line_pos - 1][pos] != 9
-        /*&& heights[line_pos - 1][pos] > current_value*/
-        {
-            next_points.push(NextPoint {
-                depth: (line_depth - 1, depth),
                 point: (line_pos - 1, pos),
             })
         }
     }
 
+    if line_pos != 0 && line_pos != (data_height - 1) {
+        if heights[line_pos + 1][pos] != 9 {
+            next_points.push(NextPoint {
+                point: (line_pos + 1, pos),
+            })
+        }
+        if heights[line_pos - 1][pos] != 9 {
+            next_points.push(NextPoint {
+                point: (line_pos - 1, pos),
+            })
+        }
+    }
 
     for next_point in next_points {
-        calculate_basin(
-            next_point.point,
-            heights,
-            next_point.clone().depth,
-            basins,
-        );
+        calculate_basin(next_point.point, heights, locations);
     }
 }
